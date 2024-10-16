3643aed010adaa2369557ec8788568b8f230432c
Torsten Kneuss <torsten@kneuss.de>
2024-10-16 21:49:49 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+#python manage.py collectstatic --noinput
 #python manage.py createsuperuser --noinput
