c608c141a3f8c7062c0fbcc5b73f9759473fd979
Torsten Kneuss <torsten@kneuss.de>
2024-10-16 18:24:10 +0200

    Setup initial page

--- page/urls.py
+++ page/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#    path('', views.MainView.as_view(), name='auditing-main'),
+    path('', views.MainView.as_view(), name='page-main'),
 ]
--- page/views.py
+++ page/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class MainView(View):
+    # noinspection PyMethodMayBeStatic
+    def get(self, request):
+
+        context = {
+        #    'response': airesponse,
+        }
+
+        return render(request, 'page/home.html', context)
